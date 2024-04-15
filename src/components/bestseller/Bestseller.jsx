9b354f9bd1f6c3b6c49ed9ab65e4676f293dd62b
lechini <110564512+lech1n@users.noreply.github.com>
2024-04-15 02:20:20 +0200

    Update Bestseller.jsx

--- src/components/bestseller/Bestseller.jsx
+++ src/components/bestseller/Bestseller.jsx
@@ -11,7 +11,7 @@ const Bestseller = () => {
 		<h3>Bestseller</h3>
 		<div className="bestseller_container">
 		{item.map((el) => (
-			<Link onClick={() => window.top(0 , 0 )} key={el.id} to={`/product/${el.id}`} style={{textDecoration:"none"}}>
+			<Link onClick={() => window.scrollTo(0 , 0 )} key={el.id} to={`/product/${el.id}`} style={{textDecoration:"none"}}>
 			<Product key={el.id} img={el.img} name={el.name} price={el.price} />
 			</Link>
 			))}
