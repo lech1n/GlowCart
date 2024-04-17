d4a2669c42342109a826eaeb05164f2434bb4966
lechini <110564512+lech1n@users.noreply.github.com>
2024-04-18 00:05:05 +0200

    Update Eyecare.jsx

--- src/pages/categories/Eyecare.jsx
+++ src/pages/categories/Eyecare.jsx
@@ -11,19 +11,20 @@
 
     return (
      <>
-     { filteredProducts.length  === 0 &&
-     (<div className="no_prod_found_container">
-      <p>No products found.</p>
-      <img src={noProd} alt="no products found" />
-      </div>
-      )}
+     
      <div className="category-name_container">
      <p>Eyecare</p>
      </div>
      {filteredProducts.length > 0 && (
       <Pagination data={filteredProducts}/>
       )}
-
+      
+      { filteredProducts.length  === 0 &&
+     (<div className="no_prod_found_container">
+      <p>No products found.</p>
+      <img src={noProd} alt="no products found" />
+      </div>
+      )}
      
      </>
      )
